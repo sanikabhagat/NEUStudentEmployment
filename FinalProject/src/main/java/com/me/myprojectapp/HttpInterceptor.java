4f7cfcb96a0e782f994175bcd2ced75064f2f67f
SanikaBhagat <bhagat.san@husky.neu.edu>
2018-04-27 05:13:14 -0400

    Http Interceptor

--- FinalProject/src/main/java/com/me/myprojectapp/HttpInterceptor.java
+++ FinalProject/src/main/java/com/me/myprojectapp/HttpInterceptor.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class HttpInterceptor extends HandlerInterceptorAdapter {
-	
+
 	public static final String ROOT_CONTEXT = "/myprojectapp/";
 
 	// before the actual handler will be executed
@@ -15,14 +15,15 @@ public class HttpInterceptor extends HandlerInterceptorAdapter {
 			throws Exception {
 
 		HttpSession session = request.getSession();
-		
+
 		System.out.println("Inside http Interceptor");
-		
-		
+
 		// Avoid a redirect loop for some urls
-		if (!request.getRequestURI().equals(ROOT_CONTEXT + "user/login.htm") && 
-				!request.getRequestURI().equals(ROOT_CONTEXT) && !request.getRequestURI().equals(ROOT_CONTEXT + "user/studentregister.htm")
-				) {
+		if (!request.getRequestURI().equals(ROOT_CONTEXT + "user/login.htm")
+				&& !request.getRequestURI().equals(ROOT_CONTEXT)
+				&& !request.getRequestURI().equals(ROOT_CONTEXT + "user/studentregister.htm")
+				&& !request.getRequestURI().equals(ROOT_CONTEXT + "user/adminregister.htm")
+				&& !request.getRequestURI().equals(ROOT_CONTEXT + "user/registeras.htm")) {
 
 			if (session.getAttribute("user") == null) {
 				response.sendRedirect("/myprojectapp/user/login.htm");
